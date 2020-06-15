8b161cff5258d7f91d941d53d056761c765c5e97
snaseer@handelsblattgroup.com <s.naseer@handelsblattgroup.com>
2020-06-16 01:12:38 +0200

    implemented getCategories from backend

---
+++ onlinebookstore/src/main/java/pk/snaseer/onlinebookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,9 @@
+package pk.snaseer.onlinebookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import pk.snaseer.onlinebookstore.entity.BookCategory;
+
+@RepositoryRestResource(collectionResourceRel="bookCategory", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
+}
